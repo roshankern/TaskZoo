b6bcd887e1dc3d94be574c305df4d17197866c01
Lavan Vivekanandasarma <lavan.vivek11@gmail.com>
2023-06-09 21:47:06 -0600

    Partially implemented last-30-day counter, implmenteed tapping card feature to display additional information

--- lib/widgets/Mainscreen/DailyTaskCard.dart
+++ lib/widgets/Mainscreen/DailyTaskCard.dart
@@ -1,8 +1,12 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const cardColor = Color.fromRGBO(175, 210, 210, 1);
 const lineColor = Color.fromRGBO(140, 146, 146, 1);
+
+//Problem add False to the queue when the task is not completed
 
 class DailyTaskCard extends StatefulWidget {
   final String title;
@@ -24,11 +28,15 @@ class DailyTaskCard extends StatefulWidget {
 class _DailyTaskCardState extends State<DailyTaskCard> {
   bool isCompleted = false;
   late DateTime previousDate;
+  final Queue<bool> completionStatusQueue = Queue<bool>();
+  bool _isTapped = false;
 
   @override
   void initState() {
     super.initState();
     previousDate = DateTime.now();
+    completionStatusQueue
+        .add(false); // Initialize the queue with a default value
   }
 
   @override
@@ -51,13 +59,32 @@ class _DailyTaskCardState extends State<DailyTaskCard> {
           previousDate.day == now.day;
     }
 
+    if (isTodayInDaysOfWeek && isCompleted) {
+      completionStatusQueue.add(true);
+      if (completionStatusQueue.length > 30) {
+        completionStatusQueue.removeFirst();
+      }
+    }
+
+    final completionCount =
+        completionStatusQueue.where((completed) => completed == true).length;
+
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: GestureDetector(
-        onLongPress: isTodayInDaysOfWeek
+        onTap: () {
+          setState(() {
+            if (!_isTapped) {
+              _isTapped = true;
+            } else {
+              _isTapped = false;
+            }
+          });
+        },
+        onLongPress: isTodayInDaysOfWeek && !_isTapped
             ? () {
                 setState(() {
-                  isCompleted = !isCompleted;
+                  isCompleted = true;
                 });
               }
             : null,
@@ -73,60 +100,71 @@ class _DailyTaskCardState extends State<DailyTaskCard> {
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Expanded(
-                  flex: 6,
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            widget.title,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: isTodayInDaysOfWeek
-                                  ? Colors.black
-                                  : lineColor,
-                              fontSize: 20.0,
+                if (!_isTapped)
+                  Expanded(
+                    flex: 6,
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              widget.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: isTodayInDaysOfWeek
+                                    ? Colors.black
+                                    : lineColor,
+                                fontSize: 20.0,
+                              ),
                             ),
-                          ),
-                          Text(
-                            widget.tag,
-                            style: TextStyle(
-                              color:
-                                  isTodayInDaysOfWeek ? Colors.grey : lineColor,
+                            Text(
+                              widget.tag,
+                              style: TextStyle(
+                                color: isTodayInDaysOfWeek
+                                    ? Colors.grey
+                                    : lineColor,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  height: 1.0,
-                  color: lineColor,
-                  margin: const EdgeInsets.symmetric(horizontal: 10.0),
-                ),
+                if (!_isTapped)
+                  Container(
+                    height: 1.0,
+                    color: lineColor,
+                    margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                  ),
                 Expanded(
                   flex: 4,
                   child: Center(
-                    child: isTodayInDaysOfWeek
-                        ? !isCompleted
-                            ? Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  const Icon(FontAwesomeIcons.clock),
-                                  const SizedBox(width: 8.0),
-                                  Text(_getTimeUntilMidnight()),
-                                ],
-                              )
-                            : const Icon(
-                                FontAwesomeIcons.check,
-                                color: Colors.black,
-                              )
-                        : Text('Relax, not for today'),
+                    child: _isTapped
+                        ? Text(
+                            'Completed ${completionCount.toString()} times in the last 30 days',
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              color: Colors.grey,
+                            ),
+                          )
+                        : isTodayInDaysOfWeek
+                            ? !isCompleted
+                                ? Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      const Icon(FontAwesomeIcons.clock),
+                                      const SizedBox(width: 8.0),
+                                      Text(_getTimeUntilMidnight()),
+                                    ],
+                                  )
+                                : const Icon(
+                                    FontAwesomeIcons.check,
+                                    color: Colors.black,
+                                  )
+                            : Text('Relax, not for today'),
                   ),
                 ),
               ],
