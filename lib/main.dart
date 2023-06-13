360c5147f9b998f8388762e20e842f8a833de0ae
Lavan Vivekanandasarma <lavan.vivek11@gmail.com>
2023-06-12 23:12:05 -0600

    Updated add task screen

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'TaskZoo',
       theme: ThemeData(
         //primaryColor is for items when selected
-        primaryColor: Colors.white,
+        primaryColor: backgroundColor,
         //scaffoldBackgroundColor is for the background
         scaffoldBackgroundColor: backgroundColor,
         //unslectedWidgetColor is for icons when unselected
@@ -56,8 +56,9 @@ class _MyHomePageState extends State<MyHomePage> {
   void _createTaskButton() async {
     final result = await showModalBottomSheet<Map<String, dynamic>>(
       context: context,
+      backgroundColor: Colors.transparent,
       builder: (context) {
-        return AddTaskSheet();
+        return const AddTaskSheet();
       },
     );
 
@@ -110,109 +111,168 @@ class _AddTaskSheetState extends State<AddTaskSheet> {
   final _tagController = TextEditingController();
   final List<bool> _daysOfWeek = List.filled(7, false);
   bool _biDaily = false;
+  bool _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).canvasColor,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20.0),
-          topRight: Radius.circular(20.0),
-        ),
-      ),
-      padding: EdgeInsets.all(16),
-      child: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextFormField(
-                maxLength: maxCharLimit,
-                controller: _titleController,
-                decoration: InputDecoration(labelText: 'Task Name'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a task name';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                maxLength: maxCharLimit,
-                controller: _tagController,
-                decoration: InputDecoration(labelText: 'Tag Name'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a tag name';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              Text('Select days of the week for the task:'),
-              SizedBox(height: 8),
-              Wrap(
-                spacing: 8.0,
-                runSpacing: 4.0,
-                alignment: WrapAlignment.spaceEvenly,
-                children: [
-                  for (int i = 0; i < 7; i++)
-                    FloatingActionButton(
-                      onPressed: () {
-                        setState(() {
-                          _daysOfWeek[i] = !_daysOfWeek[i];
-                        });
-                      },
-                      child: Text(
-                        ['M', 'T', 'W', 'T', 'F', 'S', 'S'][i],
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      backgroundColor:
-                          _daysOfWeek[i] ? Colors.green : Colors.grey,
+    return Card(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25), topRight: Radius.circular(25))),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text(
+                    'Add a New Task',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: backgroundColor,
                     ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Text('BiDaily Option:'),
-              SizedBox(height: 8),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Off'),
-                  SizedBox(width: 8),
-                  Switch(
-                    value: _biDaily,
-                    onChanged: (value) {
-                      setState(() {
-                        _biDaily = value;
-                      });
-                    },
                   ),
-                  SizedBox(width: 8),
-                  Text('On'),
+                  const SizedBox(height: 32),
+                  _buildTextField('Task Name', _titleController),
+                  const SizedBox(height: 16),
+                  _buildTextField('Tag Name', _tagController),
+                  const SizedBox(height: 16),
+                  ExpansionPanelList(
+                    elevation: 1,
+                    expandedHeaderPadding: const EdgeInsets.all(0),
+                    dividerColor: Colors.transparent,
+                    expansionCallback: (int index, bool isExpanded) =>
+                        setState(() => _isExpanded = !isExpanded),
+                    children: [
+                      ExpansionPanel(
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return const ListTile(
+                            title: Text('Additional Options',
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold)),
+                          );
+                        },
+                        body: Column(
+                          children: [
+                            _buildDaySelector(),
+                            Divider(color: Theme.of(context).dividerColor),
+                            const SizedBox(height: 16),
+                            _buildBiDailySwitch(),
+                          ],
+                        ),
+                        isExpanded: _isExpanded,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  _buildSubmitButton(context),
                 ],
               ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                child: Text('Add Task'),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    final taskData = {
-                      'title': _titleController.text,
-                      'tag': _tagController.text,
-                      'daysOfWeek': _daysOfWeek,
-                      'biDaily': _biDaily,
-                    };
-                    Navigator.pop(context, taskData);
-                  }
-                },
-              ),
-            ],
+            ),
+          ),
+        ));
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller) {
+    return TextFormField(
+      maxLength: maxCharLimit,
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(25.0),
+          borderSide: BorderSide(
+            color: Theme.of(context).primaryColor,
           ),
         ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20),
       ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a $label';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildDaySelector() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Select days of the week for the task:',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        const SizedBox(height: 8),
+        Wrap(
+          spacing: 8.0,
+          runSpacing: 4.0,
+          alignment: WrapAlignment.spaceEvenly,
+          children: [
+            for (int i = 0; i < 7; i++)
+              ChoiceChip(
+                label: Text(
+                  ['M', 'T', 'W', 'T', 'F', 'S', 'S'][i],
+                  style: TextStyle(
+                      color: _daysOfWeek[i] ? Colors.white : Colors.black),
+                ),
+                selected: _daysOfWeek[i],
+                selectedColor: Theme.of(context).primaryColor,
+                onSelected: (selected) {
+                  setState(() {
+                    _daysOfWeek[i] = selected;
+                  });
+                },
+              ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBiDailySwitch() {
+    return SwitchListTile(
+      title: const Text('BiDaily Option:',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+      value: _biDaily,
+      onChanged: (value) {
+        setState(() {
+          _biDaily = value;
+        });
+      },
+      activeColor: Theme.of(context).primaryColor,
+    );
+  }
+
+  Widget _buildSubmitButton(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Theme.of(context).primaryColor,
+        onPrimary: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 32.0),
+      ),
+      child: const Text('Add Task'),
+      onPressed: () {
+        if (_formKey.currentState!.validate()) {
+          final taskData = {
+            'title': _titleController.text,
+            'tag': _tagController.text,
+            'daysOfWeek': _daysOfWeek,
+            'biDaily': _biDaily,
+          };
+          Navigator.pop(context, taskData);
+        }
+      },
     );
   }
 }
