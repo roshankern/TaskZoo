0daad6bdbb587a6efbb9c0ab0db712f450e0cf21
roshankern <roshan.kern@ucdenver.edu>
2023-06-25 23:05:53 -0400

    update percent completed card

--- lib/pages/stats_page.dart
+++ lib/pages/stats_page.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:taskzoo/widgets/stats/pieces_progress_card.dart';
+import 'package:taskzoo/widgets/stats/daily_percent_completed_card.dart';
 
 class StatsPage extends StatelessWidget {
   const StatsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var dailyPercentCompltedTestData = {
+      "mon": 0.5,
+      "tue": 0.7,
+      "wed": 0.3,
+      "thu": 0.6,
+      "fri": 0.1,
+      "sat": 0.6,
+      "sun": 0.9,
+    };
+
     return Scaffold(
       body: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             PiecesProgressCard(
-              piecesCollected: 15, 
+              piecesCollected: 15,
               totalPieces: 20,
               circularProgressDiameter: 200,
-              circularProgressStroke : 20,
+              circularProgressStroke: 20,
               outlineStrokeWidth: 2,
-              )
-            ]
-          ),
+            ),
+            DailyPercentCompletedCard(
+              data: dailyPercentCompltedTestData,
+              width: 300,
+              height: 150,
+            )
+          ]),
       bottomNavigationBar: BottomAppBar(
         height: 50,
         color: Theme.of(context).unselectedWidgetColor,
--- lib/widgets/stats/daily_percent_completed_card.dart
+++ lib/widgets/stats/daily_percent_completed_card.dart
@@ -1,46 +1,71 @@
 import 'package:flutter/material.dart';
 
-class TaskCompletionChart extends StatelessWidget {
+class DailyPercentCompletedCard extends StatelessWidget {
   final Map<String, double> data;
+  final double height;
+  final double width;
 
-  TaskCompletionChart({required this.data});
+  DailyPercentCompletedCard(
+      {required this.data, required this.height, required this.width});
 
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      size: Size(200, 100), //This can be adjusted according to your needs
-      painter: BarChartPainter(data),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Theme.of(context).unselectedWidgetColor,
+          ),
+          child: CustomPaint(
+            size: Size(width, height + 20), // Add space for labels
+            painter: BarChartPainter(data),
+          )),
     );
   }
 }
 
 class BarChartPainter extends CustomPainter {
   final Map<String, double> data;
-  final List<Color> colors = [
-    Colors.red,
-    Colors.orange,
-    Colors.yellow,
-    Colors.green,
-    Colors.blue,
-    Colors.indigo,
-    Colors.purple,
-  ]; // Colors for each bar in the graph.
+  final Color barColor = Colors.white; // Color for each bar in the graph.
 
   BarChartPainter(this.data);
 
   @override
   void paint(Canvas canvas, Size size) {
     double barWidth = size.width / (data.keys.length * 2 - 1);
-    double maxBarHeight = size.height;
+    double maxBarHeight = size.height - 20; // Subtract space for labels
+
+    var textPainter = TextPainter(
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );
 
     int i = 0;
     for (var entry in data.entries) {
       double barHeight = maxBarHeight * entry.value;
       double left = i * barWidth * 2;
       double top = maxBarHeight - barHeight;
-      var paint = Paint()..color = colors[i % colors.length];
+      var paint = Paint()..color = barColor;
       var rect = Rect.fromLTWH(left, top, barWidth, barHeight);
       canvas.drawRect(rect, paint);
+
+      // Draw label
+      textPainter.text = TextSpan(
+        text: entry.key[0].toLowerCase(),
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 14,
+        ),
+      );
+      textPainter.layout();
+      textPainter.paint(
+          canvas,
+          Offset(
+              left + barWidth / 2 - textPainter.width / 2, maxBarHeight + 5)); // Adjust y position
+
       i++;
     }
   }
