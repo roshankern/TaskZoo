afa7091769b529d893c6e7439b5381eeec6ac7d9
roshankern <roshan.kern@ucdenver.edu>
2023-07-03 22:14:41 -0400

    refactor

--- lib/pages/zoo_page.dart
+++ lib/pages/zoo_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
 class ZooPage extends StatefulWidget {
   const ZooPage({Key? key}) : super(key: key);
 
@@ -13,46 +12,51 @@ class ZooPage extends StatefulWidget {
 }
 
 class _ZooPageState extends State<ZooPage> {
-  
   Future<Widget> loadBiomeData() async {
-  final String biomeData = await rootBundle.loadString('assets/biomes_data.json');
-  Map<String, dynamic> json = jsonDecode(biomeData);
-  
-  // Retrieve the list of animals from the JSON data
-  List<dynamic> animals = json['biomes']['arctic']['animals'];
+    final String svgData =
+        await rootBundle.loadString('assets/biomes_data/arctic/reindeer.svg');
+      print(svgData);
 
-  // Create a GridView from the list of animals
-  return GridView.builder(
-    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-    itemCount: animals.length,
-    itemBuilder: (BuildContext context, int index) {
-      return SvgPicture.asset(
-        animals[index]['svg_path'],
-        semanticsLabel: animals[index]['name'],
-      );
-    },
-  );
-}
+      return Center(child: CircularProgressIndicator());
+
+    final String biomesData =
+        await rootBundle.loadString('assets/biomes_data.json');
+    Map<String, dynamic> json = jsonDecode(biomesData);
+
+    // Retrieve the list of animals from the JSON data
+    List<dynamic> animals = json['biomes']['arctic']['animals'];
+
+    // Create a GridView from the list of animals
+    return GridView.builder(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemCount: animals.length,
+      itemBuilder: (BuildContext context, int index) {
+        return SvgPicture.asset(
+          animals[index]['svg_path'],
+          semanticsLabel: animals[index]['name'],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return FutureBuilder<Widget>(
-      future: loadBiomeData(),
-      builder: (BuildContext context, AsyncSnapshot<Widget> biomeWidgetSnapshot) {
-        if (biomeWidgetSnapshot.connectionState == ConnectionState.waiting){
-          // The JSON file is still being loaded.
-          return Center(child: CircularProgressIndicator());
-        }
-        else if (biomeWidgetSnapshot.connectionState == ConnectionState.done){
-          // The JSON file is still being loaded.
-          return biomeWidgetSnapshot.data ?? Center(child: Text('No data'));
-        }
-        else {
-          // The JSON file could not be loaded or parsed.
+        future: loadBiomeData(),
+        builder:
+            (BuildContext context, AsyncSnapshot<Widget> biomeWidgetSnapshot) {
+          if (biomeWidgetSnapshot.connectionState == ConnectionState.waiting) {
+            // The JSON file is still being loaded.
+            return Center(child: CircularProgressIndicator());
+          } else if (biomeWidgetSnapshot.connectionState ==
+              ConnectionState.done) {
+            // The JSON file is still being loaded.
+            return biomeWidgetSnapshot.data ?? Center(child: Text('No data'));
+          } else {
+            // The JSON file could not be loaded or parsed.
             return Center(child: Text('Error: ${biomeWidgetSnapshot.error}'));
-        }
-      }
-    );
+          }
+        });
   }
 }
