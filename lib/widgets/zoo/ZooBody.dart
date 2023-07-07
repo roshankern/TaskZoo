a5fec988149bffafbb351ef0748a79a50679e895
roshankern <roshan.kern@ucdenver.edu>
2023-07-07 10:25:36 -0400

    update zoo body

--- lib/widgets/zoo/ZooBody.dart
+++ lib/widgets/zoo/ZooBody.dart
@@ -11,31 +11,83 @@ class ZooBody extends StatelessWidget {
 
   ZooBody({required this.biomesData});
 
-  Widget getAnimalCard(BuildContext context, String svgPath, String animalName) {
-    return Padding(
-      padding: const EdgeInsets.all(00.0),
-      child: Container(
-        padding: const EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-          color: Theme.of(context).unselectedWidgetColor,
-          borderRadius: BorderRadius.circular(10.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 5,
-              blurRadius: 5,
+  Widget getAnimalCard(
+      BuildContext context, String svgPath, String animalName) {
+    return FutureBuilder<String>(
+      future: DefaultAssetBundle.of(context).loadString(svgPath),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final animalSvgString = snapshot.data!;
+          final fillColor = _extractFillColor(animalSvgString);
+
+          return Container(
+            decoration: BoxDecoration(
+              color: fillColor,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 5,
+                ),
+              ],
             ),
-          ],
-        ),
-        child: Center(
-          child: SvgPicture.asset(
-            svgPath,
-            semanticsLabel: animalName,
-            fit: BoxFit.contain,
-          ),
-        ),
-      ),
+            child: Center(
+              child: SvgPicture.asset(
+                svgPath,
+                semanticsLabel: animalName,
+                fit: BoxFit.contain,
+              ),
+            ),
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
     );
+
+    // return Padding(
+    //   padding: const EdgeInsets.all(8.0),
+    //   child: Container(
+    //     padding: const EdgeInsets.all(0),
+    //     decoration: BoxDecoration(
+    //       color: fillColor,
+    //       borderRadius: BorderRadius.circular(10.0),
+    //       boxShadow: [
+    //         BoxShadow(
+    //           color: Colors.grey.withOpacity(0.5),
+    //           spreadRadius: 5,
+    //           blurRadius: 5,
+    //         ),
+    //       ],
+    //     ),
+    //     child: FutureBuilder<String>(
+    //       future: DefaultAssetBundle.of(context).loadString(svgPath),
+    //       builder: (context, snapshot) {
+    //         if (snapshot.hasData) {
+    //           final svgString = snapshot.data!;
+    //           return SvgPicture.string(
+    //             svgString,
+    //             fit: BoxFit.contain,
+    //           );
+    //         } else {
+    //           return Center(child: CircularProgressIndicator());
+    //         }
+    //       },
+    //     ),
+    //   ),
+    // );
+  }
+
+  Color _extractFillColor(String animalSvgString) {
+    final fillAttributeRegex = RegExp(r'fill="([^"]+)"');
+    final match = fillAttributeRegex.firstMatch(animalSvgString);
+    final fillColor = match!.group(1);
+    final rgbValues = fillColor!.substring(4, fillColor.length - 1).split(',');
+    final red = int.parse(rgbValues[0].trim());
+    final green = int.parse(rgbValues[1].trim());
+    final blue = int.parse(rgbValues[2].trim());
+    return Color.fromRGBO(red, green, blue, 1.0);
   }
 
   @override
@@ -50,15 +102,16 @@ class ZooBody extends StatelessWidget {
     return GridView.builder(
       shrinkWrap: true,
       physics: NeverScrollableScrollPhysics(),
-      padding: EdgeInsets.all(30.0),
+      padding: EdgeInsets.all(20.0),
       itemCount: animals.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        crossAxisSpacing: 30.0,
-        mainAxisSpacing: 30.0,
+        crossAxisSpacing: 20.0,
+        mainAxisSpacing: 20.0,
       ),
       itemBuilder: (BuildContext context, int index) {
-        return getAnimalCard(context, animals[index].svgPath, animals[index].name);
+        return getAnimalCard(
+            context, animals[index].svgPath, animals[index].name);
       },
     );
   }
