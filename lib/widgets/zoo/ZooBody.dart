3f4d356f087ccb291a3f1faadb0aed0b575a78c9
roshankern <roshan.kern@ucdenver.edu>
2023-07-10 18:59:57 -0400

    use low poly fox

--- lib/widgets/zoo/ZooBody.dart
+++ lib/widgets/zoo/ZooBody.dart
@@ -11,73 +11,31 @@ class ZooBody extends StatelessWidget {
 
   ZooBody({required this.biomesData});
 
-  Widget getAnimalCard(
-      BuildContext context, String svgPath, String animalName) {
-    return FutureBuilder<String>(
-      future: DefaultAssetBundle.of(context).loadString(svgPath),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final animalSvgString = snapshot.data!;
-          final fillColor = _extractFillColor(animalSvgString);
-
-          return Container(
-            decoration: BoxDecoration(
-              color: fillColor,
-              borderRadius: BorderRadius.circular(20.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 5,
-                ),
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: SvgPicture.asset(
-                svgPath,
-                semanticsLabel: animalName,
-                fit: BoxFit.contain,
-              ),
-            ),
-          );
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
+  Widget getAnimalCard(BuildContext context, String svgPath, String animalName) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).unselectedWidgetColor,
+        borderRadius: BorderRadius.circular(20.0),
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).dividerColor.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: SvgPicture.asset(
+          svgPath,
+          semanticsLabel: animalName,
+          fit: BoxFit.contain,
+        ),
+        ),
+      ),
     );
-
-    // return Padding(
-    //   padding: const EdgeInsets.all(8.0),
-    //   child: Container(
-    //     padding: const EdgeInsets.all(0),
-    //     decoration: BoxDecoration(
-    //       color: fillColor,
-    //       borderRadius: BorderRadius.circular(10.0),
-    //       boxShadow: [
-    //         BoxShadow(
-    //           color: Colors.grey.withOpacity(0.5),
-    //           spreadRadius: 5,
-    //           blurRadius: 5,
-    //         ),
-    //       ],
-    //     ),
-    //     child: FutureBuilder<String>(
-    //       future: DefaultAssetBundle.of(context).loadString(svgPath),
-    //       builder: (context, snapshot) {
-    //         if (snapshot.hasData) {
-    //           final svgString = snapshot.data!;
-    //           return SvgPicture.string(
-    //             svgString,
-    //             fit: BoxFit.contain,
-    //           );
-    //         } else {
-    //           return Center(child: CircularProgressIndicator());
-    //         }
-    //       },
-    //     ),
-    //   ),
-    // );
   }
 
   Color _extractFillColor(String animalSvgString) {
