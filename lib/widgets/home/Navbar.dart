6657594e1b7f66498983d6663c6378acea296e6a
Roshan Kern <105665675+roshankern@users.noreply.github.com>
2023-07-21 11:54:31 -0400

    Update biomes (#10)
    * update polar bear
    
    * remove backgrounds
    
    * add ocean background
    
    * update assets
    
    * update ocean assets
    
    * update sea turtle
    
    * edit dolphin and octopus
    
    * fix stroke fills
    
    * final ocean page updates

--- lib/widgets/home/Navbar.dart
+++ lib/widgets/home/Navbar.dart
@@ -17,76 +17,69 @@ class CustomNavBar extends StatefulWidget {
 class _CustomNavBarState extends State<CustomNavBar> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        height: 50,
-        decoration: BoxDecoration(
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: SafeArea(
+        child: Container(
+          height: 50,
           color: Theme.of(context).primaryColor,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 5,
-              blurRadius: 7,
-              offset: Offset(0, 3), // changes position of shadow
-            ),
-          ],
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  color: Theme.of(context).indicatorColor,
-                  icon: const Icon(Icons.pie_chart_outline),
-                  onPressed: () {
-                    widget.onTap(0);
-                  },
-                  iconSize: widget.otherIconSize,
-                ),
-                IconButton(
-                  color: Theme.of(context).indicatorColor,
-                  icon: const Icon(Icons.settings_outlined),
-                  onPressed: () {
-                    widget.onTap(3);
-                  },
-                  iconSize: widget.otherIconSize,
-                ),
-              ],
-            ),
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   IconButton(
-                    color: widget.currentIndex == 1
-                        ? Theme.of(context).indicatorColor
-                        : Theme.of(context).dividerColor,
-                    icon: const Icon(Icons.circle),
+                    color: Theme.of(context).indicatorColor,
+                    icon: const Icon(Icons.pie_chart_outline),
                     onPressed: () {
-                      widget.onTap(1);
+                      widget.onTap(0);
                     },
-                    iconSize: widget.dotIconSize,
-                    padding: EdgeInsets.all(5),
-                    constraints: BoxConstraints(),
+                    iconSize: widget.otherIconSize,
                   ),
                   IconButton(
-                    color: widget.currentIndex == 2
-                        ? Theme.of(context).indicatorColor
-                        : Theme.of(context).dividerColor,
-                    icon: const Icon(Icons.circle),
+                    color: Theme.of(context).indicatorColor,
+                    icon: const Icon(Icons.settings_outlined),
                     onPressed: () {
-                      widget.onTap(2);
+                      widget.onTap(3);
                     },
-                    iconSize: widget.dotIconSize,
-                    padding: EdgeInsets.all(5),
-                    constraints: BoxConstraints(),
+                    iconSize: widget.otherIconSize,
                   ),
                 ],
               ),
-            ),
-          ],
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      color: widget.currentIndex == 1
+                          ? Theme.of(context).indicatorColor
+                          : Theme.of(context).dividerColor,
+                      icon: const Icon(Icons.circle),
+                      onPressed: () {
+                        widget.onTap(1);
+                      },
+                      iconSize: widget.dotIconSize,
+                      padding: EdgeInsets.all(5),
+                      constraints: BoxConstraints(),
+                    ),
+                    IconButton(
+                      color: widget.currentIndex == 2
+                          ? Theme.of(context).indicatorColor
+                          : Theme.of(context).dividerColor,
+                      icon: const Icon(Icons.circle),
+                      onPressed: () {
+                        widget.onTap(2);
+                      },
+                      iconSize: widget.dotIconSize,
+                      padding: EdgeInsets.all(5),
+                      constraints: BoxConstraints(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
