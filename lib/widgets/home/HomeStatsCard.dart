e8615b1978f265d6e1d19a0788d7d78358599793
Lavan Vivekanandasarma <lavan.vivek11@gmail.com>
2023-08-17 18:24:55 -0600

    Cleaned up redundant code from CustomAnimatedDigitWidget

--- lib/widgets/home/HomeStatsCard.dart
+++ lib/widgets/home/HomeStatsCard.dart
@@ -1,8 +1,6 @@
-import 'package:animated_digit/animated_digit.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:dimensions_theme/dimensions_theme.dart';
-import 'package:provider/provider.dart';
 import 'package:taskzoo/misc/theme_notifier.dart';
 import 'package:taskzoo/widgets/isar_service.dart';
 import 'package:taskzoo/widgets/stats/custom_animated_digit_widget.dart'; // Import your ThemeNotifier class
@@ -83,49 +81,17 @@ class _HomeStatsCardState extends State<HomeStatsCard> {
           children: [
             icon,
             const SizedBox(width: 5.0),
-            if (snapshot.hasData) _buildThemeStreamBuilder(snapshot.data!),
+            if (snapshot.hasData)
+              CustomAnimatedDigitWidget(
+                  value: snapshot.data!,
+                  textColor:
+                      widget.themeNotifier.currentTheme == ThemeMode.light
+                          ? Colors.black
+                          : Colors.white,
+                  themeNotifier: widget.themeNotifier),
           ],
         );
       },
     );
-  }
-
-  StreamBuilder<int> _buildThemeStreamBuilder(int value) {
-    return StreamBuilder<int>(
-      stream: widget.service.preferenceStream("theme"),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Container(); // Return an empty container if there's no data yet
-        }
-
-        final color = snapshot.data == 1 ? Colors.blue : Colors.red;
-
-        return Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const SizedBox(width: 5.0),
-            ColoredWidget(
-                color,
-                value,
-                widget
-                    .themeNotifier), // Replace 'yourData' with your actual data source
-          ],
-        );
-      },
-    );
-  }
-}
-
-class ColoredWidget extends StatelessWidget {
-  final Color color;
-  int value;
-  ThemeNotifier themeNotifier;
-
-  ColoredWidget(this.color, this.value, this.themeNotifier);
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomAnimatedDigitWidget(
-        value: value, textColor: color, themeNotifier: themeNotifier);
   }
 }
